552e1e6c493d90e8e6eddefeee097563dd977c89
roman <roman@example.ee>
2020-09-21 09:41:22 +0300

    9 задание

---
+++ WebWorkers/fibonachiWorker.js
@@ -0,0 +1,14 @@
+function calcFib(x) {
+	if (x > 1) {
+		return calcFib(x - 1) + calcFib(x - 2);
+	}
+	else {
+		return x;
+	}
+}
+
+addEventListener('message', function () {
+	for (var i = 0; i < 50; i++) {
+		postMessage(i + ' = ' + calcFib(i) + '<br />');
+	}
+}, false);
---
+++ WebWorkers/task2.js
@@ -0,0 +1,15 @@
+addEventListener('message', function (e) {
+	var data = e.data;
+
+	switch (data.cmd) {
+		case 'start':
+			postMessage('ПОТОК ЗАПУЩЕН: ' + data.msg);
+			break;
+		case 'stop':
+			postMessage('ПОТОК ОСТАНОВЛЕН: ' + data.msg);			
+			close();
+			break;
+		default:
+			postMessage('НЕИЗВЕСТНАЯ КОМАНДА: ' + data.msg);
+	}
+}, true);
